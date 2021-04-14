4bf29cbccbee54845b504fa763e63871ebf9ed04
GOKUL PRASATH E <gokul.prasath@TVSSCS.COM>
2021-04-14 15:05:57 +0530

    app

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from .views import (createPatient, patientDetails, updatePatient,
-                    deletePatient, listPatient, getPatientsList, createPatientAjax, listPatientAjax)
+                    deletePatient, listPatient, getPatientsList, createPatientAjax, listPatientAjax,
+                    patientDetailsAjax, updatePatientAjax)
 
 
 urlpatterns = [
     # path('', views.home, name="Blog-home"),
     path('add/', createPatient, name="add-patient"),
-    path('detail/', patientDetails, name="patient-details"),
-    path('edit/', updatePatient, name="update-patient"),
+    # path('detail/', patientDetails, name="patient-details"),
+    # path('edit/', updatePatient, name="update-patient"),
     path('deletePatient/', deletePatient, name="delete-patient"),
     path('view/', listPatient, name="list-patient"),
     path('ajax_view/', getPatientsList, name="ajax-list-patient"),
     path('ajax_add/', createPatientAjax, name="ajax-add-patient-page"),
     path('ajax_patient_list/', listPatientAjax, name="ajax-patient-list-page"),
-
+    path('detail/', patientDetailsAjax, name="ajax-patient-detail-page"),
+    path('edit/', updatePatientAjax, name="ajax-patient-update-page"),
 ]
--- core/views.py
+++ core/views.py
@@ -14,23 +14,6 @@ def createPatient(request):
     return redirect('list-patient')
 
 
-def createPatientAjax(request):
-    context = {}
-    if request.method == 'POST':
-        try:
-            v_first_name = request.POST.get('first_name')
-            v_last_name = request.POST.get('last_name')
-            v_date_of_birth = datetime.strptime(
-                request.POST.get('date_of_birth'), '%m/%d/%Y').date()
-            patient_obj, created = Patient.objects.get_or_create(
-                first_name=v_first_name, last_name=v_last_name, date_of_birth=v_date_of_birth)
-            return JsonResponse({'status': 'success'})
-        except:
-            return JsonResponse({'status': 'failed'})
-    html = render_to_string('core/add_patient.html', context, request=request)
-    return JsonResponse({'html': html, 'url': '/add/'})
-
-
 def patientDetails(request):
     if request.method == 'GET':
         try:
@@ -101,3 +84,64 @@ def getPatientsList(request):
         context['patient_list'] = Patient.objects.all().order_by('first_name')
         html = render_to_string('core/patient_table.html', context, request=request)
         return JsonResponse({'table_html': html})
+
+
+def createPatientAjax(request):
+    context = {}
+    if request.method == 'POST':
+        try:
+            v_first_name = request.POST.get('first_name')
+            v_last_name = request.POST.get('last_name')
+            v_date_of_birth = datetime.strptime(
+                request.POST.get('date_of_birth'), '%m/%d/%Y').date()
+            patient_obj, created = Patient.objects.get_or_create(
+                first_name=v_first_name, last_name=v_last_name, date_of_birth=v_date_of_birth)
+            return JsonResponse({'status': 'success'})
+        except:
+            return JsonResponse({'status': 'failed'})
+    html = render_to_string('core/add_patient.html', context, request=request)
+    return JsonResponse({'html': html, 'url': '/add/'})
+
+
+def patientDetailsAjax(request):
+    if request.method == 'GET':
+        try:
+            patient_id = request.GET.get('patient_id')
+            patient_obj = Patient.objects.get(pk=patient_id)
+            context = {}
+            context['patient'] = patient_obj
+            html = render_to_string('core/patient_details.html', context, request=request)
+            print('inside pp')
+            return JsonResponse({'html': html})
+        except Patient.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    else:
+        return JsonResponse({})
+
+
+def updatePatientAjax(request):
+    if request.method == 'GET':
+        try:
+            patient_id = request.GET.get('patient_id')
+            patient_obj = Patient.objects.get(pk=patient_id)
+            context = {}
+            context['patient'] = patient_obj
+            html = render_to_string('core/edit_patient_details.html', context, request=request)
+            return JsonResponse({'html': html})
+        except Patient.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    elif request.method == 'POST':
+        try:
+            print('inside form update')
+            patient_id = int(request.POST.get('patient_id'))
+            patient_obj = Patient.objects.get(pk=patient_id)
+            patient_obj.first_name = request.POST.get('first_name')
+            patient_obj.date_of_birth = datetime.strptime(
+                request.POST.get('date_of_birth'), '%m/%d/%Y').date()
+            patient_obj.gender = request.POST.get('gender')
+            patient_obj.save()
+            return JsonResponse({})
+        except Patient.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    else:
+        return JsonResponse({})
